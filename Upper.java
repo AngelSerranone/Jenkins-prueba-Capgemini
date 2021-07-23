1f83dd348b6ebadc2f271ccecfb464c9984bee9f
Angel Serrano <69475464+AngelSerranone@users.noreply.github.com>
2021-07-23 10:18:42 +0200

    Create Upper.java

---
+++ Upper.java
@@ -0,0 +1,9 @@
+public class Upper {
+
+    public static void main(String args[]) { 
+        String nombre = args[0];
+        nombre = nombre.toUpperCase();
+        System.out.println(nombre);
+    }
+
+}
